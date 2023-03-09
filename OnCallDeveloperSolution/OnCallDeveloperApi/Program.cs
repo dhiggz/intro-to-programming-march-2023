cdebd441d1c13b8a678850279e190264922b02cd
dhiggz <higgindw@miamioh.edu>
2023-03-09 16:30:04 -0500

    unit testing code from end of thursday

--- OnCallDeveloperSolution/OnCallDeveloperApi/Program.cs
+++ OnCallDeveloperSolution/OnCallDeveloperApi/Program.cs
@@ -1,3 +1,5 @@
+using OnCallDeveloperApi;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,8 +8,8 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddSingleton<IProvideTheBusinessClock, TwentyFourHourBusinessClock>();
+builder.Services.AddSingleton<ISystemTime, SystemTime>();
+builder.Services.AddSingleton<IProvideTheBusinessClock, StandardBusinessClock>();
 
 var app = builder.Build();
 
