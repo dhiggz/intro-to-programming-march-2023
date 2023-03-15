a335f72f3497ea8b8ac7b40e45bef6f95bb9c21c
dhiggz <higgindw@miamioh.edu>
2023-03-15 12:27:35 -0400

    before lunch

--- frontend/src/app/features/counter/state/effects/counter.effects.ts
+++ frontend/src/app/features/counter/state/effects/counter.effects.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Actions, concatLatestFrom, createEffect, ofType } from '@ngrx/effects';
 import { Store } from '@ngrx/store';
-import { map, tap, filter } from 'rxjs';
+import { z } from 'zod';
+import { map, tap, filter, catchError, of } from 'rxjs';
 import { selectCounterBranch } from '..';
 import {
   counterCommands,
@@ -12,6 +13,14 @@ import { CounterState } from '../reducers/counter.reducer';
 
 @Injectable()
 export class CounterEffects {
+  private readonly CountDataSchema = z.object({
+    current: z.number(),
+    by: z.union([
+      z.literal(1),
+      z.literal(3),
+      z.literal(5),
+    ]),
+  });
   // logItAll$ = createEffect(
   //   () => {
   //     return this.actions$.pipe(tap((action) => console.log(action.type))); // =>
@@ -25,10 +34,16 @@ export class CounterEffects {
       ofType(counterCommands.loadCounterState),
       map(() => localStorage.getItem('counter-state')),
       filter((storedValue) => storedValue !== null),
-      map((theString) => JSON.parse(theString!) as CounterState),
+      map((theString) => JSON.parse(theString!)),
+      map((susObject) => this.CountDataSchema.parse(susObject) as CounterState),
       map((counterState) =>
         counterDocuments.counterState({ payload: counterState }),
       ),
+      catchError(() => {
+        console.log('We have ourselves a hacker here!');
+        localStorage.clear();
+        return of({ type: 'Localstorage Error' });
+      }),
     );
   });
 
